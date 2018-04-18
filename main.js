0ccb52449f0e96d477fa51eaafbe81f128319426
chiamaka <obitubealex@gmail.com>
2018-04-18 17:25:18 +0100

    ALC 2.0 Take the climb challenge: Complete Address Book Build

---
+++ main.js
@@ -0,0 +1,85 @@
+// Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+// Save Bookmark
+function saveBookmark(e){
+  // Get form values
+  var siteName =document.getElementById('siteName').value;
+  var siteUrl =document.getElementById('siteUrl').value;
+  var siteEmail =document.getElementById('siteEmail').value;
+
+  var bookmark = {
+    name: siteName,
+    url: siteUrl,
+    email: siteEmail
+  }
+
+  // Test if bookmarks is null
+  if(localStorage.getItem('bookmarks') === null){
+    // Init array
+    var bookmarks = [];
+    // Add to array
+    bookmarks.push(bookmark);
+    // Set to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+    // Get bookmarks from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // Add bookmark to array
+    bookmarks.push(bookmark);
+    // Re-set back to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+
+  // Clear form
+  document.getElementById('myForm').reset();
+
+  // Re-fetch bookmarks
+  fetchBookmarks();
+
+  // Prevent form from submitting
+  e.preventDefault();
+}
+
+// Delete bookmark
+function deleteBookmark(url){
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // Loop through the bookmarks
+  for(var i =0;i < bookmarks.length;i++){
+    if(bookmarks[i].url == url){
+      // Remove from array
+      bookmarks.splice(i, 1);
+    }
+  }
+  // Re-set back to localStorage
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  // Re-fetch bookmarks
+  fetchBookmarks();
+}
+
+// Fetch bookmarks
+function fetchBookmarks(){
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // Get output id
+  var bookmarksResults = document.getElementById('bookmarksResults');
+
+  // Build output
+  bookmarksResults.innerHTML = '';
+  for(var i = 0; i < bookmarks.length; i++){
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+    var email = bookmarks[i].email;
+
+    bookmarksResults.innerHTML += '<div class="row">'+
+                                    '<p>'+
+                                    '<div class="col-md-3">'+name+'</div>'+
+                                    '<div class="col-md-3">'+url+'</div>'+
+                                    '<div class="col-md-3">'+email+'</div>'+
+                                    '<div class="col-md-3"><a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a></div>'+
+                                    '</p>'+
+                                  '</div>';
+  }
+}
